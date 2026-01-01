932e4fff41cb090c6d4e380673ff2818f0b9d1ab
rufinomendoza <ram46@georgetown.edu>
2026-01-01 13:49:05 -0500

    Code split Home, BackStage, Not Found, Philanthropy, and Store Views

---
+++ src/views/index.js
@@ -0,0 +1,6 @@
+export { default as HomeView } from './HomeView';
+export { default as StoreView } from './StoreView';
+export { default as BackstageView } from './BackstageView';
+export { default as NotFoundView } from './NotFoundView';
+export { default as PhilanthropyView } from './PhilanthropyView';
+// Add others as you extract them
